9c0f210a222082fcd1fc5a42a20d40990de4157c
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-15 00:01:43 -0800

    navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import BookList from "./Component/BookList";
+import Navbar from "./Component/Navbar";
 
 function App() {
   return (
     <div className="App">
       <BookList/>
+      <Navbar/>
     </div>
   );
 }
